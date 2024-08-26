61450782c76d4b62d268f783c42dec23113cab59
assaf <assafperetz6@gmail.com>
2024-08-26 15:22:14 +0300

    Add basic caching mechanism

--- Countries/js/controllers/countries.controller.js
+++ Countries/js/controllers/countries.controller.js
@@ -3,20 +3,14 @@
 function onGetCountryInfo() {
     const elTxtInput = document.querySelector('input')
 
-    getCountryInfo(elTxtInput.value, renderInfo)
+    getCountryByName(elTxtInput.value, renderInfo)
 }
 
 function renderInfo(rawData) {
     const elCountryInfo = document.querySelector('pre')
     const { name, capital, borders, maps, flags } = rawData[0]
-    
-    // const formatedData = 
 
     elCountryInfo.innerText = [name.common, capital[0], ...borders, maps.googleMaps, flags.svg].join(' ')
     console.log()
     
-}
-
-function getCountryInfo(country, cb) {
-    $.get(`https://restcountries.com/v3.1/name/${country}`, cb)
 }
